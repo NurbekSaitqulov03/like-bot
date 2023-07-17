5020862d450d00633e106891027cda238e1d607c
JavohirJalilov <javohirjaliloff@gmail.com>
2023-07-17 10:15:14 +0500

    create inline button

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import telegram
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import os
 
 TOKEN=os.environ['TOKEN']
 
 def main(update, context):
+    bot = context.bot
+
     chat_id = update.message.chat.id
     text = update.message.text
-    print(text)
-    bot = context.bot
-    bot.sendMessage(chat_id=chat_id, text=text)
+    button1 = InlineKeyboardButton(text='👍', callback_data="like_emoji")
+    button2 = InlineKeyboardButton(text='👎', callback_data="dislike_emoji")
+    keyboard = InlineKeyboardMarkup([[button1, button2]])
+
+    bot.sendMessage(chat_id=chat_id, text=text, reply_markup=keyboard)
+
+def count(update, context):
+    query = update.callback_query
+    print(query.data)
     
 updater = Updater(TOKEN)
 dp = updater.dispatcher
 
 dp.add_handler(MessageHandler(Filters.text, main))
+dp.add_handler(CallbackQueryHandler(count))
 
 updater.start_polling()
 updater.idle()
